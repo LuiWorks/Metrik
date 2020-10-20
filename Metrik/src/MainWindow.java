5bd6f33246126ccd55ae6de30806e189ad513f21
IchStudent <u35148@hs-harz.com>
2020-10-20 15:46:59 +0200

    Version 1.0 Opens a Frame, clicking on it will draw a circle at the clicked spot. At every 3rd click the two drawn circles will disappear.

---
+++ Metrik/src/MainWindow.java
@@ -0,0 +1,62 @@
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainWindow extends Frame implements WindowListener {
+	
+	public MainWindow() {
+		setTitle("Programm für Aufgabe 1 in Softwaretechnik Verison 1.0");
+		setSize(700, 700); 
+		addWindowListener(this);
+		add(new Drawing()); 
+
+	}
+	
+	public void openWindow() {
+		setVisible(true);
+	}
+
+	
+	
+	
+	
+	@Override
+	public void windowActivated(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		dispose();
+		System.exit(0);
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		
+		
+	}
+
+}
